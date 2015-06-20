0a420878c6502bd8568586d20e8ffa45726d1915
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-06-20 19:21:43 +0200

    Console application that pings InfluxDB and checks the version.

---
+++ InfluxDB.Net.Collector.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InfluxDB.Net.Collector.Console
+{
+    static class Program
+    {
+        private static InfluxDb _client;
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                //url:      http://128.199.43.107:8086
+                //username: root
+                //Password: ?????
+                throw new InvalidOperationException("Three parameters needs to be provided to this application. url, username and password for the InfluxDB database.");
+            }
+
+            var url = args[0];
+            var username = args[1];
+            var password = args[2];
+
+            _client = new InfluxDb(url, username, password);
+
+            var pong = _client.PingAsync().Result;
+            System.Console.WriteLine("Ping: {0} ({1} ms)", pong.Status, pong.ResponseTime);
+
+            var version = _client.VersionAsync().Result;
+            System.Console.WriteLine("Version: {0}", version);
+
+            System.Console.WriteLine("Press any key to exit...");
+            System.Console.ReadKey();
+        }
+    }
+}
