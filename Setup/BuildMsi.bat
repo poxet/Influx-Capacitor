576f65d7bc0f70f5e53bd442cdb9d005ad6a1a11
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-09-08 08:19:42 +0200

    Updated script to build msi package

--- Setup/BuildMsi.bat
+++ Setup/BuildMsi.bat
@@ -1,3 +1,3 @@
-for /f "delims=" %%i in ('"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\uuidgen.exe"') do set output=%%i
-"C:\Program Files (x86)\WiX Toolset v3.9\bin\candle.exe" Influx-Capacitor.wsx -dEnvironment="Release" -dVersion="1.0.8" -dId="%output%"
+for /f "delims=" %%i in ('"C:\Program Files (x86)\Windows Kits\8.1\bin\x64\uuidgen.exe"') do set guid=%%i
+"C:\Program Files (x86)\WiX Toolset v3.9\bin\candle.exe" Influx-Capacitor.wsx -dEnvironment="%2" -dVersion="%1" -dId="%guid%"
 "C:\Program Files (x86)\WiX Toolset v3.9\bin\light.exe" -ext WixUIExtension -cultures:en-us Influx-Capacitor.wixobj
