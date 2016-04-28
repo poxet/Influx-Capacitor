e0742b69cb5a8abacfe5b3a6d278da3f942a76dc
Colton Fussy <confusingboat@gmail.com>
2016-04-27 23:44:09 -0500

    add option to allow duplicate PerformanceCounters instance names

--- Tharga.Influx-Capacitor.Collector/Entities/CounterGroup.cs
+++ Tharga.Influx-Capacitor.Collector/Entities/CounterGroup.cs
@@ -13,10 +13,11 @@ namespace Tharga.InfluxCapacitor.Collector.Entities
         private readonly List<ICounter> _counters;
         private readonly List<ITag> _tags;
         private readonly CollectorEngineType _collectorEngineType;
-        private readonly IEnumerable<ICounterInstanceFilter> _filters ;
+        private readonly IEnumerable<ICounterInstanceFilter> _filters;
         private readonly string _provider;
+        private readonly bool _allowDuplicateInstanceNames;
 
-        public CounterGroup(string name, int secondsInterval, int refreshInstanceInterval, List<ICounter> counters, IEnumerable<ITag> tags, CollectorEngineType collectorEngineType, IEnumerable<ICounterInstanceFilter> filters, string provider)
+        public CounterGroup(string name, int secondsInterval, int refreshInstanceInterval, List<ICounter> counters, IEnumerable<ITag> tags, CollectorEngineType collectorEngineType, IEnumerable<ICounterInstanceFilter> filters, string provider, bool allowDuplicateInstanceNames)
         {
             _name = name;
             _secondsInterval = secondsInterval;
@@ -26,6 +27,7 @@ namespace Tharga.InfluxCapacitor.Collector.Entities
             _collectorEngineType = collectorEngineType;
             _filters = filters;
             _provider = provider;
+            _allowDuplicateInstanceNames = allowDuplicateInstanceNames;
         }
 
         public string Name { get { return _name; } }
@@ -34,7 +36,8 @@ namespace Tharga.InfluxCapacitor.Collector.Entities
         public IEnumerable<ICounter> Counters { get { return _counters; } }
         public IEnumerable<ITag> Tags { get { return _tags; } }
         public CollectorEngineType CollectorEngineType { get { return _collectorEngineType; } }
-        public IEnumerable<ICounterInstanceFilter> Filters { get { return _filters; } } 
+        public bool AllowDuplicateInstanceNames { get { return _allowDuplicateInstanceNames; } }
+        public IEnumerable<ICounterInstanceFilter> Filters { get { return _filters; } }
         public string Provider { get { return _provider; } }
     }
 }
--- Tharga.Influx-Capacitor.Collector/Handlers/DataInitiator.cs
+++ Tharga.Influx-Capacitor.Collector/Handlers/DataInitiator.cs
@@ -26,7 +26,7 @@ namespace Tharga.InfluxCapacitor.Collector.Handlers
 
         public Tuple<string, Tuple<string, OutputLevel>> CreateCounter(string groupName, int secondsInterval, int refreshInstanceInterval, List<ICounter> counters, CollectorEngineType collectorEngineType)
         {
-            var response = new CounterGroup(groupName, secondsInterval, refreshInstanceInterval, counters, new ITag[] { }, collectorEngineType, null, null);
+            var response = new CounterGroup(groupName, secondsInterval, refreshInstanceInterval, counters, new ITag[] { }, collectorEngineType, null, null, false);
             var message = CreateFile(groupName, response);
             return new Tuple<string, Tuple<string, OutputLevel>>(groupName, message);
         }
@@ -36,7 +36,7 @@ namespace Tharga.InfluxCapacitor.Collector.Handlers
             var name = "processor";
 
             var counters = new List<ICounter> { new Counter("Processor", "% Processor Time", "*", null, null, null, null, null) };
-            var response = new CounterGroup(name, 10, 0, counters, new ITag[] { }, CollectorEngineType.Safe, null, null);
+            var response = new CounterGroup(name, 10, 0, counters, new ITag[] { }, CollectorEngineType.Safe, null, null, false);
             return ConvertErrorsToWarnings(CreateFile(name, response));
         }
 
@@ -45,7 +45,7 @@ namespace Tharga.InfluxCapacitor.Collector.Handlers
             var name = "memory";
             
             var counters = new List<ICounter> { new Counter("Memory", "*", string.Empty, null, null, null, null, null) };
-            var response = new CounterGroup(name, 10, 0, counters, new ITag[] { }, CollectorEngineType.Safe, null, null);
+            var response = new CounterGroup(name, 10, 0, counters, new ITag[] { }, CollectorEngineType.Safe, null, null, false);
             return ConvertErrorsToWarnings(CreateFile(name, response));
         }
 
